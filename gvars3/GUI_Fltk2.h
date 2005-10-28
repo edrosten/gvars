5b9dbe4355bc8a8b828302538c62e0b0f5ac1833
gr281 <gr281>
2005-10-28 13:26:37 +0000

    added fltk2 based implementation

---
+++ gvars3/GUI_Fltk2.h
@@ -0,0 +1,74 @@
+#ifndef __GUI_FLTK2_H
+#define __GUI_FLTK2_H
+
+#include <gvars3/config.h>
+
+#ifdef GV3_HAVE_FLTK2
+#include <gvars3/GUI.h>
+#include <pthread.h>
+
+#include <map>
+#include <set>
+#include <string>
+
+namespace GVars3
+{
+class GUI_Fltk2_win;
+
+class GUI_Fltk2
+{
+	public:
+		GUI_Fltk2(GUI *pGUI, GVars2 *pGV2);
+		static const int widget_height = 25;
+		static const int widget_padding_x = 5;
+		static const int widget_padding_y = 2;
+
+		void   poll_windows();
+		void   process_in_crnt_thread();
+		void   start_thread();
+
+	private:
+		void InitXInterface(std::string sDisplay);
+		void AddMonitor(std::string cmd, std::string args);
+		void AddWindow(std::string sParams);
+		void DestroyWindow(std::string sCommand);
+		void AddPushButton(std::string cmd, std::string args);
+		void AddToggleButton(std::string cmd, std::string args);
+		void AddSlider(std::string cmd, std::string args);
+		void AddSpin(std::string cmd, std::string args);
+
+		std::string remove_suffix(std::string cmd, std::string suffix);
+		bool   check_window(std::string win_name, std::string fname);
+
+
+
+		GUI 	*gui;
+		GVars2	*gv2;
+		bool	init;
+		std::string	name;
+		pthread_t gui_thread;
+
+		typedef struct
+		{
+			GUI_Fltk2_win*	win;
+			bool			showme;
+		} window;
+
+
+		std::map<std::string, window> windows;
+
+		static void InitXInterfaceCB(void*, std::string, std::string);
+		static void AddWindowCB(void*, std::string, std::string);
+		static void DestroyWindowCB(void*, std::string, std::string);
+		static void AddPushButtonCB(void*, std::string, std::string);
+		static void AddToggleButtonCB(void*, std::string, std::string);
+		static void AddSliderCB(void*, std::string, std::string);
+		static void AddMonitorCB(void*, std::string, std::string);
+		static void AddSpinCB(void*, std::string, std::string);
+		static void* do_stuff_CB(void*);
+};
+
+}
+
+#endif
+#endif
--- gvars3/instances.h
+++ gvars3/instances.h
@@ -1,5 +1,6 @@
 #include <gvars3/GUI.h>
 #include <gvars3/GUI_Fltk.h>
+#include <gvars3/GUI_Fltk2.h>
 #include <gvars3/GUI_Motif.h>
 #include <gvars3/config.h>
 
@@ -11,6 +12,10 @@ namespace GVars3
 	#ifdef GV3_HAVE_FLTK
 		extern class GUI_Fltk GUI_Fltk;
 	#endif
+
+    #ifdef GV3_HAVE_FLTK2
+        extern class GUI_Fltk2 GUI_Fltk2;
+    #endif
 
 	#ifdef GV3_HAVE_MOTIF
 	extern GUI_Motif GUI_Motif;
